3cbd21f548abb519727b52bd5eb9adca20f58858
altafziyaa <altafziyaa91@gmail.com>
2026-01-29 16:32:11 +0530

    update cart quantity

--- cart-service/src/controller/cart.controller.js
+++ cart-service/src/controller/cart.controller.js
@@ -4,8 +4,10 @@ class cartController {
   async addToCart(req, res, next) {
     const { userId, product } = req.body;
     try {
-      if (!userId) res.status(400).json({ message: "user id not valid" });
-      if (!product) res.status(400).json({ message: "product not exist" });
+      if (!userId)
+        return res.status(400).json({ message: "user id not valid" });
+      if (!product)
+        return res.status(400).json({ message: "product not exist" });
 
       const addCart = await cartService.addToCart({
         userId,
@@ -15,6 +17,28 @@ class cartController {
         success: true,
         message: "product added to cart successfully",
         addCart,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateCartQuantity(req, res, next) {
+    const { userId, productId, quantity } = req.body;
+    try {
+      if (!userId) return res.status(400).json({ message: "user id required" });
+      if (!productId)
+        return res.status(400).json({ message: "product is required" });
+      const updateCart = cartService.updateCartQuantity({
+        userId,
+        productId,
+        quantity,
+      });
+
+      return res.status(200).json({
+        success: true,
+        message: "update cart successfully",
+        updateCart,
       });
     } catch (error) {
       next(error);
